7a5591e821235c1133fda9e29a30265cc5870f79
Victor Santana <santanavictor592@gmail.com>
2023-12-22 14:44:21 -0300

    feat: Adicionando o projeto BMICalculator

--- src/informations/listProjects.ts
+++ src/informations/listProjects.ts
@@ -1,14 +1,15 @@
 import DevSteam from "../assets/images/devsteam-preview.jpg";
 import Pokedex from "../assets/images/pokedex-preview.jpg";
 import Megaflix from "../assets/images/megaflix-preview.jpg";
+import BMICalculator from "../assets/images/bmi-calculator-preview.jpg";
 
 const listProjects = [
   {
     id: Math.floor(Math.random() * 1_000_000_000),
     image: DevSteam,
     alt: "Imagem prévia DevSteam",
-    width: 300,
-    height: 374,
+    width: 200,
+    height: 249,
     title: "DevSteam",
     about: `Este é o projeto de um e-commerce baseado na Steam.`,
     stack: ["React", "Typescript", "Javascript", "HTML5", "CSS3"],
@@ -19,8 +20,8 @@ const listProjects = [
     id: Math.floor(Math.random() * 1_000_000_000),
     image: Pokedex,
     alt: "Imagem prévia Pokédex",
-    width: 1366,
-    height: 675,
+    width: 200,
+    height: 292,
     title: "Pokédex",
     about: `Projeto de uma Pokédex que consome a PokéAPI e exibe alguns dados do pokémon
     pesquisado.`,
@@ -32,13 +33,25 @@ const listProjects = [
     id: Math.floor(Math.random() * 1_000_000_000),
     image: Megaflix,
     alt: "Imagem prévia Megaflix",
-    width: 167,
-    height: 374,
+    width: 200,
+    height: 488,
     title: "Megaflix",
     about: `Este projeto consome a API do TMDB e contém a funcionalidade de pesquisar filmes.`,
     stack: ["React", "Typescript", "Javascript", "HTML5", "CSS3"],
     linkGitHub: "https://github.com/Santana-Victor/megaflix",
     linkProject: "https://megaflix-nu.vercel.app/",
+  },
+  {
+    id: Math.floor(Math.random() * 1_000_000_000),
+    image: BMICalculator,
+    alt: "Imagem prévia Calculadora de IMC",
+    width: 200,
+    height: 138,
+    title: "Calculadora de IMC",
+    about: `Projeto de uma calculadora de IMC.`,
+    stack: ["React", "Typescript", "Javascript", "HTML5", "CSS3"],
+    linkGitHub: "https://github.com/Santana-Victor/bmi-calculator",
+    linkProject: "https://santana-victor.github.io/bmi-calculator/",
   },
 ];
 
