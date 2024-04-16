459a382ba9b96402550169b91de2ac6dc095a9c5
Victor Santana <santanavictor592@gmail.com>
2024-04-16 15:32:55 -0300

    feat: add SectionProjectsComponent

---
+++ src/app/components/section-projects/section-projects.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-projects.component.html',
+  styleUrl: './section-projects.component.scss',
+})
+export class SectionProjectsComponent {}
