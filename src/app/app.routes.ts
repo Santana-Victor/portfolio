2527bdaa7c312086ddde3540ecfef11760570050
Victor Santana <santanavictor592@gmail.com>
2024-04-16 15:56:11 -0300

    feat: implementando as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsPageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
